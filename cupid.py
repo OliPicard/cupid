83474a8234d6273dbb1abb515eaf4a7fc593146d
Oliver Bryant <oliver@ufomail.co.uk>
2015-05-01 17:49:03 +0000

    string io method
    Thanks to diminonten for providing an in memory version of the csv
    saving tool. credits have been added.

--- cupid.py
+++ cupid.py
@@ -2,36 +2,29 @@ __author__ = 'OliPicard'
 import csv
 import json
 import sys
+from io import StringIO
 import requests
 
 '''a csv to json converter written for the UKStationLive schema
-run this tool via cmd> cupid.py station.csv
+simply run the tool to get the station.json output.
 Thank you to the following
+excalibur (for his streaming data save copy)
+diminonten (for his memory version)
 '''
 
 def cupid():
-    csv_file = requests.get('http://www.nationalrail.co.uk/static/documents/content/station_codes.csv', stream=True)
-    if csv_file.status_code == requests.codes.ok:
+    response = requests.get('http://www.nationalrail.co.uk/static/documents/content/station_codes.csv')
+    if response.status_code == requests.codes.ok:
         print("Grabbing file from National Rail")
-        with open('station_codes.csv', 'wb') as e:
-            for chunk in csv_file.iter_content(chunk_size=1024):
-                if chunk:
-                    e.write(chunk)
-                    e.flush()
-    try:
-        t = open('station_codes.csv', 'r')
-    except IOError:
-        print('Unable to get data. delete the file and try again!')
-        sys.exit()
+        csv_file = StringIO(response.content.decode())
     headers = ['Station Name', 'Code']
-    csv_reader = csv.DictReader(t, headers)
+    csv_reader = csv.DictReader(csv_file, headers)
     json_filename = 'station.json'
     print('Saving the file', json_filename)
     jsonf = open(json_filename, 'w')
     data = {r['Station Name']: r['Code'] for r in csv_reader}
     redata = json.dumps(data, indent=4, sort_keys=True)
     jsonf.write(redata)
-    e.close()
     jsonf.close()
 
 if __name__ == "__main__":
